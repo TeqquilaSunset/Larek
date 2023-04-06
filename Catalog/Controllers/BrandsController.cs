ea793760c10118a879b7e002c095c3dfbdef1603
Vasiliy <sc-2005@yandex.ru>
2023-04-06 19:45:10 +0700

    feat(): Init Catalog

---
+++ Catalog/Controllers/BrandsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Catalog;
+using Catalog.Models;
+
+namespace Catalog.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BrandsController : Controller
+    {
+        ApplicationContext db = new ApplicationContext();
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            var brands = db.Brands.ToList();
+            return new JsonResult(brands);
+        }
+
+        [HttpPost]
+        public JsonResult Post(Brand newBrand)
+        {
+            if (newBrand == null)
+            {
+                return new JsonResult(BadRequest());
+            }
+
+            db.Brands.Add(newBrand);
+            db.SaveChanges();
+            return new JsonResult(newBrand);
+        }
+        
+    }
+}
