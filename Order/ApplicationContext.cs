2f00307a51bfca6f06e41931a7998a8559822ed8
TeqquilaSunset <86839875+TeqquilaSunset@users.noreply.github.com>
2023-04-08 22:58:58 +0700

    Feat ProductOrder (#1)
    * feat():init
    
    * feat(csproj): folders
    
    * feat(): add Models
    
    * feat(): Add GET POST DELETE
    
    * del(): del OrderDto

---
+++ Order/ApplicationContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using ProductOrder.Models;
+
+namespace ProductOrder
+{
+    public class ApplicationContext : DbContext
+    {
+
+        public DbSet<Order> Orders { get; set; }
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=order.db");
+        }
+    }
+
+}
---
+++ Order/Models/Order.cs
@@ -0,0 +1,16 @@
+﻿using ProductOrder.Models;
+
+namespace ProductOrder.Models
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerEmail { get; set; }
+        public string CustomerPhone { get; set; }
+        public string CustomerAdress { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int Quantity { get; set; }
+        public Guid ProductId { get; set; }
+    }
+}
